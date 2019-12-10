b3825d26b91fc94d1bd1562afa9102d5965cd66d
DKomaristiy <DKomaristiy@yandex.ru>
2019-12-10 14:40:01 +0300

    add lab_4

---
+++ projects/lab_4/task/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H__
+#define MAIN_H__
+
+#include <stm32f4xx.h>
+
+/* PWM frequency */
+#define PWM_FREQUENCY_HZ      50
+
+void configure_buttons_0(void);
+void configure_buttons_1(void);
+void SetSysClockTo84(void);
+
+
+
+
+#endif
